356bc88363dfdc99302938e1a52f4548916998a8
Ausin Formica <austin.formica@gmail.com>
2015-03-27 15:33:37 -0400

    ticker icon toggle

--- js/menu.js
+++ js/menu.js
@@ -30,6 +30,7 @@ setInterval(function() {
     $('#ticker-message').fadeOut(50, function() {
         var $this = $(this);
         $this.text($this.text() == "Find the iron that's right for you. Get Fit!" ? 'FREE SHIPPING On All Orders Over $100' : "Find the iron that's right for you. Get Fit!");
-        $this.fadeIn(500);  $('.fa-cog').toggleClass('fa-twitter');
+        $this.fadeIn(500);
+        $('.fa-cog').toggleClass('fa-twitter');
     });
 }, 5000);
