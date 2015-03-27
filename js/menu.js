55632eba83bf0f6f39428c6a6f78de794339e856
Ausin Formica <austin.formica@gmail.com>
2015-03-27 13:01:47 -0400

    jq nav menu fade

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,3 @@
-// Foundation JavaScript
-// Documentation can be found at: http://foundation.zurb.com/docs
-$(document).foundation();
 $(document).ready(function(){
   $("#sticker").sticky({topSpacing:0});
 });
